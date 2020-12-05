cb1ad9351e813ca9347765d05290c1e182103e31
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2020-12-05 14:11:36 -0800

    new uri

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGO || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
